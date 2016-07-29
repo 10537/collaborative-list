d7c6efbb46fbd06f6297df92acc34554ac763b01
Jose Moreno <yosefeliyahu@gmail.com>
2016-07-28 22:08:53 -0500

    [IMP]Improve css for delete <a> tag. [IMP]Changed desing of the Itemboard.'

--- client/boards/BoardItem.jsx
+++ client/boards/BoardItem.jsx
@@ -13,8 +13,8 @@ export default class BoardItem extends Component {
 
   render () {
     return (
-      <div className="row" onClick={this.goToBoard.bind(this)}>
-        <div className="item-title col-sm-12">
+      <div className="row">
+        <div className="item-title col-sm-12" onClick={this.goToBoard.bind(this)}>
           {this.props.boarditem.name}
           <div className="row">
             <div className="col-xs-4 col-sm-4">
@@ -23,16 +23,14 @@ export default class BoardItem extends Component {
             <div className="col-xs-4 col-sm-4">
               Users
             </div>
-            <div className="col-xs-3 col-sm-3">
-              Task Number: 120
-            </div>
-            <div className="col-xs-1 col-sm-1">
-              <a href="#" className="pull-right" onClick={this.deleteBoard.bind(this)}>
-                <i className="fa fa-trash fa-3x"></i>
-              </a>
+            <div className="col-xs-4 col-sm-4">
+              Task Number: 124
             </div>
           </div>
         </div>
+          <a href="#" className="pull-right delete-button" onClick={this.deleteBoard.bind(this)}>
+            <i className="fa fa-trash fa-2x"></i>
+          </a>
       </div>
     )
 
